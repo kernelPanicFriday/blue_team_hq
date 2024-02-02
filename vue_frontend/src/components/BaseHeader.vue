b54c7dd5a198b1915db570cdd7b6863f5f3d2473
kernelPanicFriday <tyler.evrhett@gmail.com>
2024-02-02 15:57:56 -0500

    created base page with VERY basic styles applied, ned to make some design decisions here

---
+++ vue_frontend/src/components/BaseHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="bg-gray-600">
+        <nav>
+            <div class="flex flex-auto">
+                <a href='#' v-for="(link, idx) in links" :key="idx">{{ link }}</a>
+            </div>
+        </nav>    
+    </header>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+let links = reactive(['Home', 'Hire me', 'buy me stuff'])
+
+// Need to implement router for these buttons
+
+</script>
---
+++ vue_frontend/src/components/BasePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <base-header></base-header>
+<!-- implement my base card here next pr -->
+</template>
+
+<script>
+import BaseHeader from './BaseHeader.vue'
+
+//Should I swap out active components with a slot. make design decision here on next pr
+
+export default {
+  components: {
+    BaseHeader
+  }
+}
+</script>
+<style>
+</style>
