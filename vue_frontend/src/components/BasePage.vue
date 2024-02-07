be8780be5dd0fdb130bf9a11078debc534e0821e
kernelPanicFriday <tyler.evrhett@gmail.com>
2024-02-07 10:07:22 -0500

    dynamic components

--- vue_frontend/src/components/BasePage.vue
+++ vue_frontend/src/components/BasePage.vue
@@ -1,28 +1,40 @@
 <template>
   <base-header></base-header>
-  <button @click="setActiveCard">click</button>
-
-  <about-me v-if="activeCard=='about_me'"></about-me>
-  <info-data v-else></info-data>
+  <button v-for="(tab, idx) in tabs" :key="idx" @click="setActiveTab(tab)"> {{ tab }} </button>
+  <div v-if="isLoading">LOADING!</div>
+  <info-card v-else :data=activeTabData[activeTab] :key="activeTabData"></info-card>
 </template>
 
 <script setup>
-import BaseHeader from './BaseHeader.vue'
-import AboutMe from './pages/AboutMe.vue'
-import InfoData from './pages/InfoData.vue'
-import { ref } from 'vue';
+import BaseHeader from './BaseHeader.vue';
+import InfoCard from './pages/InfoCard.vue';
+import { reactive, ref, onMounted } from 'vue';
 
-let activeCard = ref('about_me')
+const tabs = ref(['About Me', 'Contact Me']);
+const activeTabData = reactive({});
+const activeTab = ref(tabs.value[0]);
+const isLoading = ref(true);
 
-function setActiveCard(){
-  console.log('clicked')
-  if (activeCard.value === 'about_me') {
-    activeCard.value = 'info_data'
-  } else {
-    activeCard.value = 'about_me'
-  }
-  console.log(activeCard)
+function setActiveTab(tab){
+  console.log('clicked', tab)
+  activeTab.value = tab
+  console.log(activeTab.value)
 }
+
+function getTabData(){
+  // TODO: make this an async fetch('/my/backend/url') adn add error handling
+  console.debug('Fetching data...')
+
+  //but for right now
+  activeTabData['About Me'] = 'Hey this is a bunch of data about me'
+  activeTabData['Contact Me'] = 'Here is how you can contact me '
+  isLoading.value = false;
+  console.debug('Success')
+}
+
+//grab all data to populate the tab data
+onMounted(() => getTabData());
+
 
 </script>
 
